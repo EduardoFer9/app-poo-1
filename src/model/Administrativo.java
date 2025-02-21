319b2c20041cc232001cfa0a09e979ada3b00f5b
EduardoFer9 <oscacito009@gmail.com>
2025-02-20 20:16:20 -0500

    Aplicando polimorfismo a la clase administrativo

--- src/model/Administrativo.java
+++ src/model/Administrativo.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Administrativo extends Persona{
+
+
+    @Override
+    public boolean registrarAsistencia(String codIdentificador, String fechaAsistencia, String HoraAsistencia, String valorAsistencia) {
+        System.out.println("Aplicar logica" +
+                "para la asistencia" +
+                "administrativo");
+        return true;
+
+    }
 }
